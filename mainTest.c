0fc8d03c4cb6a08bfedfb0d8c740513887ae270c
jnguyen9218 <jnguyen9218@sdsu.edu>
2025-04-28 19:31:22 -0700

    Create mainTest.c
    Added test option for data3.txt

---
+++ mainTest.c
@@ -0,0 +1,464 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+// Global variables for static sizes
+#define PHYSICAL_PAGES 32
+#define PAGE_TABLE_ENTRIES 128
+#define OFFSET_BITS 9
+#define REFERENCE_RESET_INTERVAL 200
+
+// Page Table Entry struct
+typedef struct {
+    int frame_num;
+    int present;
+    int dirty;
+    int referenced;
+} PTE;
+
+// Frame in physical memory struct
+typedef struct {
+    int pid;
+    int virtual_pn; //virtual page number
+    int load_time;
+    int last_acc_time;
+    int dirty;
+} FRAME;
+
+// Function to extract virtual page number and offset from a virtual address
+void PAGE_OFFSET(int virtual_addr, int *page_num, int *offset) {
+    *page_num = virtual_addr >> OFFSET_BITS; // shift right by 9 bits
+    *offset = virtual_addr & ((1 << OFFSET_BITS) - 1);
+}
+
+// Function to simulate the Random (RAND) page replacement algorithm
+void RAND(FILE *fp) {
+    PTE page_tables[10][PAGE_TABLE_ENTRIES] = {0}; // Assuming max 10 processes
+    FRAME physical_memory[PHYSICAL_PAGES] = {0};
+    int next_aframe, rand_pf, rand_ref, rand_dw, ref_count= 0; // aframe = available frame
+    char line[256];
+
+    // Initialize random seed
+    srand(1); // seed set to 1 so it can be replicated
+
+    // Set process ID to -1 to mark frames as empty
+    for (int i = 0; i < PHYSICAL_PAGES; i++) {
+        physical_memory[i].pid = -1; 
+    }
+
+    printf("\n Random (RAND) Algorithm: \n");
+
+    // Read each line of file fp
+    while (fgets(line, sizeof(line), fp)) {
+        int pid, virtual_addr;
+        char access_type;
+        if (sscanf(line, "%d %d %c", &pid, &virtual_addr, &access_type) != 3) {
+            continue;
+        }
+        ref_count++;
+
+        int virtual_pn, offset;
+        PAGE_OFFSET(virtual_addr, &virtual_pn, &offset);
+        
+        // Check if page is present
+        if (!page_tables[pid][virtual_pn].present) {
+            rand_pf++; // if page not present, count page fault
+            rand_ref++; // count disk reference
+
+            // If there are still empty frames to use
+            int use_frame;
+            if (next_aframe < PHYSICAL_PAGES) {
+                use_frame = next_aframe++;
+            } else {
+                use_frame = rand() % PHYSICAL_PAGES;
+                if (physical_memory[use_frame].pid != -1 &&
+                    page_tables[physical_memory[use_frame].pid][physical_memory[use_frame].virtual_pn].dirty) {
+                    rand_ref++;
+                    rand_dw++;
+                    page_tables[physical_memory[use_frame].pid][physical_memory[use_frame].virtual_pn].dirty = 0;
+                }
+                page_tables[physical_memory[use_frame].pid][physical_memory[use_frame].virtual_pn].present = 0;
+            }
+
+            // Update page table for the new page
+            page_tables[pid][virtual_pn].present = 1;
+            page_tables[pid][virtual_pn].frame_num = use_frame;
+            page_tables[pid][virtual_pn].dirty = (access_type == 'W');
+            page_tables[pid][virtual_pn].referenced = 1;
+
+            // Update physical memory
+            physical_memory[use_frame].pid = pid;
+            physical_memory[use_frame].virtual_pn = virtual_pn;
+            physical_memory[use_frame].dirty = (access_type == 'W');
+        } else { //verify corresponding entry 
+            page_tables[pid][virtual_pn].referenced = 1;
+            if (access_type == 'W') {
+                page_tables[pid][virtual_pn].dirty = 1;
+                int frame_num = page_tables[pid][virtual_pn].frame_num;
+                physical_memory[frame_num].dirty = 1;
+            }
+            int frame_num = page_tables[pid][virtual_pn].frame_num;
+        }
+
+        if (ref_count == REFERENCE_RESET_INTERVAL) {
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < PAGE_TABLE_ENTRIES; j++) {
+                    page_tables[i][j].referenced = 0;
+                }
+            }
+            ref_count = 0;
+        }
+    }
+
+    printf("Total Page Faults: %d\n", rand_pf);
+    printf("Total Disk References: %d\n", rand_ref);
+    printf("Total Dirty Page Writes: %d\n", rand_dw);
+    rewind(fp); // Reset file pointer for the next simulation
+}
+
+// Funtion to simulate the First In First Out (FIFO) page replacement algorithm
+void FIFO(FILE*fp){
+    PTE page_tables[10][PAGE_TABLE_ENTRIES] = {0};
+    FRAME physical_memory[PHYSICAL_PAGES] = {0};
+    int next_aframe = 0, FIFO_pf = 0, FIFO_ref = 0, FIFO_dw =0, ref_count =0, oldest_frame = 0;
+    char line[256];
+
+    printf("\n FIFO Algorithm: \n");
+
+    while (fgets(line, sizeof(line), fp)){
+        int pid, virtual_addr;
+        char access_type;
+        if (sscanf(line, "%d %d %c", &pid, &virtual_addr, &access_type) != 3) {
+            continue;
+        }
+        ref_count++;
+
+        int virtual_pn, offset;
+        PAGE_OFFSET(virtual_addr, &virtual_pn, &offset);
+    
+        if (!page_tables[pid][virtual_pn].present) {
+            FIFO_pf++; // add page fault
+            FIFO_ref++; // add page reference
+
+            int use_frame;
+            if (next_aframe < PHYSICAL_PAGES) {
+                use_frame = next_aframe++;
+            } else {
+                use_frame = oldest_frame;
+                if (physical_memory[use_frame].pid != -1 &&
+                    page_tables[physical_memory[use_frame].pid][physical_memory[use_frame].virtual_pn].dirty) {
+                    FIFO_ref++;
+                    FIFO_dw++;
+                    page_tables[physical_memory[use_frame].pid][physical_memory[use_frame].virtual_pn].dirty = 0;
+                }
+                page_tables[physical_memory[use_frame].pid][physical_memory[use_frame].virtual_pn].present = 0;
+
+                // Next oldest frame (+1) since oldest one was just used
+                oldest_frame = (oldest_frame + 1) % PHYSICAL_PAGES;
+            }
+
+            // Update page table for the new page
+            page_tables[pid][virtual_pn].present = 1;
+            page_tables[pid][virtual_pn].frame_num = use_frame;
+            page_tables[pid][virtual_pn].dirty = (access_type == 'W');
+            page_tables[pid][virtual_pn].referenced = 1;
+
+            // Update physical memory
+            physical_memory[use_frame].pid = pid;
+            physical_memory[use_frame].virtual_pn = virtual_pn;
+            physical_memory[use_frame].dirty = (access_type == 'W');
+        } else { 
+            // Page already present
+            page_tables[pid][virtual_pn].referenced = 1;
+            if (access_type == 'W') {
+                page_tables[pid][virtual_pn].dirty = 1;
+                int frame_num = page_tables[pid][virtual_pn].frame_num;
+                physical_memory[frame_num].dirty = 1;
+            }
+        }
+
+        if (ref_count == REFERENCE_RESET_INTERVAL) {
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < PAGE_TABLE_ENTRIES; j++) {
+                    page_tables[i][j].referenced = 0;
+                }
+            }
+            ref_count = 0;
+        }
+    }
+
+    printf("Total Page Faults: %d\n", FIFO_pf);
+    printf("Total Disk References: %d\n", FIFO_ref);
+    printf("Total Dirty Page Writes: %d\n", FIFO_dw);
+    rewind(fp);
+}
+
+
+// Function to simulate the Least Recently Used (LRU) page replacement algorithm
+void LRU(FILE *fp) {
+    PTE page_tables[10][PAGE_TABLE_ENTRIES] = {0};
+    FRAME physical_memory[PHYSICAL_PAGES] = {0};
+    int next_available_frame = 0, LRU_pf = 0, LRU_references = 0, LRU_dw = 0, reference_count = 0, current_time = 0;
+    char line[256];
+
+    for (int i = 0; i < PHYSICAL_PAGES; i++) {
+        physical_memory[i].pid = -1;
+    }
+
+    printf("\n LRU Algorithm:  \n");
+
+    while (fgets(line, sizeof(line), fp)) {
+        int process_id, virtual_address;
+        char access_type;
+        if (sscanf(line, "%d %d %c", &process_id, &virtual_address, &access_type) != 3) {
+            continue;
+        }
+        current_time++;
+        reference_count++;
+
+        int virtual_page_number, offset;
+        PAGE_OFFSET(virtual_address, &virtual_page_number, &offset);
+
+        if (!page_tables[process_id][virtual_page_number].present) {
+            LRU_pf++; // fault check
+            LRU_references++;
+
+            int frame_to_use;
+            if (next_available_frame < PHYSICAL_PAGES) {
+                frame_to_use = next_available_frame++;
+            } else { //LRU page location
+                int lru_frame = -1; 
+                int min_last_used = current_time + 1; 
+                for (int i = 0; i < PHYSICAL_PAGES; i++) {
+                    if (physical_memory[i].pid != -1 && physical_memory[i].last_acc_time < min_last_used) {
+                        min_last_used = physical_memory[i].last_acc_time;
+                        lru_frame = i;
+                    }
+                }
+
+                if (lru_frame != -1) {
+                    frame_to_use = lru_frame;
+                    if (physical_memory[frame_to_use].dirty) {
+                        LRU_references++;
+                        LRU_dw++;
+                        page_tables[physical_memory[frame_to_use].pid][physical_memory[frame_to_use].virtual_pn].dirty = 0;
+                    }
+                    page_tables[physical_memory[frame_to_use].pid][physical_memory[frame_to_use].virtual_pn].present = 0;
+                } else {
+                    fprintf(stderr, "Error: No LRU frame found for replacement\n");
+                    exit(1);
+                }
+            }
+
+            page_tables[process_id][virtual_page_number].present = 1;
+            page_tables[process_id][virtual_page_number].frame_num = frame_to_use;
+            page_tables[process_id][virtual_page_number].dirty = (access_type == 'W');
+            page_tables[process_id][virtual_page_number].referenced = 1;
+
+            physical_memory[frame_to_use].pid = process_id;
+            physical_memory[frame_to_use].virtual_pn = virtual_page_number;
+            physical_memory[frame_to_use].last_acc_time = current_time;
+            physical_memory[frame_to_use].dirty = (access_type == 'W');
+        } else {
+            page_tables[process_id][virtual_page_number].referenced = 1;
+            if (access_type == 'W') {
+                page_tables[process_id][virtual_page_number].dirty = 1;
+                int frame_num = page_tables[process_id][virtual_page_number].frame_num;
+                physical_memory[frame_num].dirty = 1;
+            }
+            int frame_num = page_tables[process_id][virtual_page_number].frame_num;
+            physical_memory[frame_num].last_acc_time = current_time;
+        }
+
+        if (reference_count == REFERENCE_RESET_INTERVAL) {
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < PAGE_TABLE_ENTRIES; j++) {
+                    page_tables[i][j].referenced = 0;
+                }
+            }
+            reference_count = 0;
+        }
+    }
+
+    printf("Total Page Faults: %d\n", LRU_pf);
+    printf("Total Disk References: %d\n", LRU_references);
+    printf("Total Dirty Page Writes: %d\n", LRU_dw);
+    rewind(fp); // Reset file pointer for the next simulation
+}
+
+// Function to simulate the Periodic Reference Reset (PER) page replacement algorithm
+void PER(FILE *fp) {
+    PTE page_tables[10][PAGE_TABLE_ENTRIES] = {0};
+    FRAME physical_memory[PHYSICAL_PAGES] = {0};
+    int next_aframe=0, PER_pf=0, PER_ref=0, PER_dw=0, ref_count=0, curr_time= 0;
+    char line[256];
+
+    for (int i = 0; i < PHYSICAL_PAGES; i++) {
+        physical_memory[i].pid = -1;
+    }
+
+    printf("\n Periodic Reference Reset (PER) Algorithm: \n");
+
+    while (fgets(line, sizeof(line), fp)) {
+        int pid, virtual_addr;
+        char access_type;
+        if (sscanf(line, "%d %d %c", &pid, &virtual_addr, &access_type) != 3) {
+            continue;
+        }
+        curr_time++;
+        ref_count++;
+
+        int virtual_pn, offset;
+        PAGE_OFFSET(virtual_addr, &virtual_pn, &offset);
+
+        if (!page_tables[pid][virtual_pn].present) {
+            PER_pf++; // Page Fault
+            PER_ref++;
+
+            int use_frame;
+            if (next_aframe < PHYSICAL_PAGES) {
+                use_frame = next_aframe++;
+            } else {
+                int check_frame = -1; //locate page
+
+                for (int i = 0; i < PHYSICAL_PAGES; i++) { //identify unallocated pages
+                    if (physical_memory[i].pid == -1) {
+                        check_frame = i;
+                        break;
+                    }
+                }
+
+                if (check_frame == -1) {
+                    for (int i = 0; i < PHYSICAL_PAGES; i++) { //check for unreference and clean 
+                        if (physical_memory[i].pid != -1 &&
+                            !page_tables[physical_memory[i].pid][physical_memory[i].virtual_pn].referenced &&
+                            !page_tables[physical_memory[i].pid][physical_memory[i].virtual_pn].dirty) {
+                            check_frame = i;
+                            break;
+                        }
+                    }
+                }
+
+                if (check_frame == -1) {
+                    for (int i = 0; i < PHYSICAL_PAGES; i++) { //check for unferenced and dirty
+                        if (physical_memory[i].pid != -1 &&
+                            !page_tables[physical_memory[i].pid][physical_memory[i].virtual_pn].referenced &&
+                            page_tables[physical_memory[i].pid][physical_memory[i].virtual_pn].dirty) {
+                            check_frame = i;
+                            break;
+                        }
+                    }
+                }
+
+                if (check_frame == -1) {
+                    for (int i = 0; i < PHYSICAL_PAGES; i++) { //check for referenced and clean
+                        if (physical_memory[i].pid != -1 &&
+                            page_tables[physical_memory[i].pid][physical_memory[i].virtual_pn].referenced &&
+                            !page_tables[physical_memory[i].pid][physical_memory[i].virtual_pn].dirty) {
+                            check_frame = i;
+                            break;
+                        }
+                    }
+                }
+
+                if (check_frame == -1) {
+                    for (int i = 0; i < PHYSICAL_PAGES; i++) { //check for referenced and dirty
+                        if (physical_memory[i].pid != -1 &&
+                            page_tables[physical_memory[i].pid][physical_memory[i].virtual_pn].referenced &&
+                            page_tables[physical_memory[i].pid][physical_memory[i].virtual_pn].dirty) {
+                            check_frame = i;
+                            break;
+                        }
+                    }
+                }
+
+                if (check_frame != -1) {
+                    use_frame = check_frame;
+                    if (physical_memory[use_frame].dirty) {
+                        PER_ref++;
+                        PER_dw++;
+                        page_tables[physical_memory[use_frame].pid][physical_memory[use_frame].virtual_pn].dirty = 0;
+                    }
+                    page_tables[physical_memory[use_frame].pid][physical_memory[use_frame].virtual_pn].present = 0;
+                } else {
+                    fprintf(stderr, "Error: No victim frame found for PER replacement\n");
+                    exit(1);
+                }
+            }
+
+            page_tables[pid][virtual_pn].present = 1;
+            page_tables[pid][virtual_pn].frame_num = use_frame;
+            page_tables[pid][virtual_pn].dirty = (access_type == 'W');
+            page_tables[pid][virtual_pn].referenced = 1;
+
+            physical_memory[use_frame].pid = pid;
+            physical_memory[use_frame].virtual_pn = virtual_pn;
+            physical_memory[use_frame].last_acc_time = curr_time;
+            physical_memory[use_frame].dirty = (access_type == 'W');
+        } else {
+            page_tables[pid][virtual_pn].referenced = 1;
+            if (access_type == 'W') {
+                page_tables[pid][virtual_pn].dirty = 1;
+                int frame_num = page_tables[pid][virtual_pn].frame_num;
+                physical_memory[frame_num].dirty = 1;
+            }
+            int frame_num = page_tables[pid][virtual_pn].frame_num;
+            physical_memory[frame_num].last_acc_time = curr_time;
+        }
+
+        if (ref_count == REFERENCE_RESET_INTERVAL) {
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < PAGE_TABLE_ENTRIES; j++) {
+                    page_tables[i][j].referenced = 0;
+                }
+            }
+            ref_count = 0;
+        }
+    }
+
+    printf("Total Page Faults: %d\n", PER_pf);
+    printf("Total Disk References: %d\n", PER_ref);
+    printf("Total Dirty Page Writes: %d\n", PER_dw);
+    rewind(fp);
+}
+
+int main() {
+    char filename[20];
+    int data;
+    FILE *fp;
+
+    printf("Select a data file to open:\n");
+    printf("1. data1.txt\n");
+    printf("2. data2.txt\n");
+    printf("3. data3.txt\n");
+    printf("Enter desired dataset (1, 2, or 3): ");
+   // printf("Enter desired dataset (1 or 2): ");
+    scanf("%d", &data);
+
+    if (data == 1) {
+        strcpy(filename, "data1.txt");
+    } else if (data == 2) {
+        strcpy(filename, "data2.txt");
+    } else if (data == 3) { // Our test file to verify
+        strcpy(filename, "data3.txt");
+    } else {
+        printf("Invalid Selection!!!\n");
+        return 1;
+    }
+
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        perror("Error Opening File!!!");
+        return 1;
+    }
+
+    // Prompt functions to simulation for algorithms
+    RAND(fp);
+    FIFO(fp);
+    LRU(fp);
+    PER(fp);
+
+    fclose(fp);
+    return 0;
+}
